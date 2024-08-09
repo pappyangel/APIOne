82b9d9c2075a16d7efa022d7f752d6e153b69d20
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-08-09 13:52:27 +0000

    chore: Update Index.cshtml to include environment information

--- frontend/Pages/Index.cshtml.cs
+++ frontend/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace frontend.Pages
@@ -11,14 +12,20 @@ namespace frontend.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+        public  string envEnvironment  { get; set; }
+        public string envAPIProductionUrl { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public void OnGet()
         {
+            envEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            envAPIProductionUrl = _config["APIProductionUrl"];
 
         }
     }
